5fdbbb8042c8bb7401119d84de7ac97c45d450d1
lwq <460737391@qq.com>
2020-05-05 14:48:50 +0800

    李伟琪的第一次上传

---
+++ src/main/java/edu/zsc/todolistproject/controller/notebookController.java
@@ -0,0 +1,11 @@
+package edu.zsc.todolistproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class notebookController {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
